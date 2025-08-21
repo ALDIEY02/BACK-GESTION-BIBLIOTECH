6f16ac78c81ad1d80ad5b5f287f9baef2c47cfed
lengolo <holndou359@gmail.com>
2025-08-21 11:19:14 +0000

    First commit

---
+++ src/main/java/com/example/BACK_GESTION_BIBLIOTECH/BackGestionBibliotechApplication.java
@@ -0,0 +1,13 @@
+package com.example.BACK_GESTION_BIBLIOTECH;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackGestionBibliotechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackGestionBibliotechApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BACK_GESTION_BIBLIOTECH/BackGestionBibliotechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BACK_GESTION_BIBLIOTECH;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackGestionBibliotechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
